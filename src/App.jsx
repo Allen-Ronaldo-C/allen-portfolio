2d26b5382421c775802c4554202a8af86c141f73
Allen-Ronaldo-C <145185616+Allen-Ronaldo-C@users.noreply.github.com>
2026-03-09 00:30:58 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -332,7 +332,7 @@ function Hero() {
         </div>
         <div className="hero-btns">
           <a href="#projects" className="btn btn-cyan">🚀 View Projects</a>
-          <a href="/Allen_Ronaldo_Resume(1).pdf" className="btn btn-outline" target="_blank" rel="noreferrer">📄 Download Resume</a>
+          <a href="./assests/Allen_Ronaldo_Resume(1).pdf" className="btn btn-outline" target="_blank" rel="noreferrer">📄 Download Resume</a>
           <a href="#contact" className="btn btn-ghost">📬 Contact Me</a>
         </div>
         <div className="hero-pills">
@@ -398,7 +398,7 @@ function About() {
                 <div className="about-photo-inner">
                   {!imageError ? (
                     <img 
-                      src="/photo.png" 
+                      src=".\assets\1755326708636.jpg" 
                       alt="Allen Ronaldo C"
                       className="about-photo-img"
                       onError={() => setImageError(true)}
